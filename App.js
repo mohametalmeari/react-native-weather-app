05dbee2ee833cd8a6576ea75766b37ef9df014fa
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-03 22:45:19 +0300

    Clean up App.js

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 
-export default function App() {
-  console.log('App executed')
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>Current Weather</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: 'pink',
+    flex: 1
   }
 })
+export default App
