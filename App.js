4e0378718b365bef36e4fd1d99ffdf432bcdae2b
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-03 23:04:02 +0300

    Create weather template

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its Sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
       </View>
     </SafeAreaView>
   )
@@ -13,11 +22,37 @@ const App = () => {
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1
+    flex: 1,
+    backgroundColor: 'pink'
   },
   container: {
-    backgroundColor: 'pink',
-    flex: 1
+    flex: 1,
+    alignItems: 'center'
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
   }
 })
 export default App
