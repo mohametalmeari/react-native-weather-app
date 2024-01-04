dd3f38afbaaa32ba08bb9e521d2467a566540ed5
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-04 10:42:57 +0300

    Create Tabs

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
 import React from 'react'
-import { View, StyleSheet, SafeAreaView } from 'react-native'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
 import { NavigationContainer } from '@react-navigation/native'
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
+const Tab = createBottomTabNavigator()
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <CurrentWeather />
-      </View>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen
+          name={'Current'}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="droplet"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'Upcoming'}
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="clock"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'City'}
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="home"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 export default App
