f544c5b7a1877bd0406fe9aae0ec190bd4f97a88
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-04 10:27:29 +0300

    Import NavigationContainer into App

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { View, StyleSheet, SafeAreaView } from 'react-native'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather />
+      </View>
+    </NavigationContainer>
   )
 }
 
