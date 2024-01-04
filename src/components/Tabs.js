7428151f6983521a8e2ebf31927a1cc25e364281
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-05 01:51:40 +0300

    Style Tabs

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -12,7 +12,18 @@ const Tabs = () => {
     <Tab.Navigator
       screenOptions={{
         tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray'
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: 'lightblue'
+        },
+        headerStyle: {
+          backgroundColor: 'lightblue'
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+          color: 'tomato'
+        }
       }}
     >
       <Tab.Screen
