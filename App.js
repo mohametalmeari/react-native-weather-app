3158af5cf1c80cc02b1f857a6c5b61decfdb8c0d
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-05 18:04:12 +0300

    Implement loading icon

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
@@ -10,4 +20,10 @@ const App = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 export default App
