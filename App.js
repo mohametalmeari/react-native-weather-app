95170d9f07a4599e8310334b8622a7aeb408cd5b
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-06 18:29:15 +0300

    Install moment

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
-import * as Location from 'expo-location'
-import { WEATHER_API_KEY } from '@env'
 import { useGetWeather } from './src/hooks/useGetWeather'
 
 const App = () => {
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { weatherType } from '../utilities/weatherType'
+import moment from 'moment'
+
 const ListItem = (props) => {
   const { dt_txt, min, max, condition } = props
-  const { item, temp, date } = styles
+  const { item, temp, date, dateTextWrapper } = styles
   return (
     <View style={item}>
       <Feather name={weatherType[condition].icon} size={50} color="white" />
-      <Text style={date}>{dt_txt}</Text>
+      <View style={dateTextWrapper}>
+        <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
+        <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
+      </View>
       <Text style={temp}>{`${Math.round(min)}°/${Math.round(max)}°`}</Text>
     </View>
   )
@@ -32,6 +37,9 @@ const styles = StyleSheet.create({
   date: {
     color: 'white',
     fontSize: 15
+  },
+  dateTextWrapper: {
+    flexDirection: 'column',
   }
 })
 export default ListItem
